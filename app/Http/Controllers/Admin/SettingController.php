d26ba56d96fc7909841f2cb667969f7e27772416
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-15 18:01:11 +0100

    Created a File called a UploadAble Traits

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Http\Controllers\Admin;
 
+use App\Traits\UploadAble;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 
 class SettingController extends BaseController
 {
-
+    use UploadAble;
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
