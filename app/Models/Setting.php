3aa335d8d147c88c4fa21052a2e2a674d422d5eb
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-15 12:22:05 +0100

    Updated the .git which was corrupted

---
+++ app/Models/Setting.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use League\Flysystem\Config;
+
+class Setting extends Model
+{
+    /**
+     * @var string
+     */
+
+    protected  $table = 'settings';
+
+    /**
+     * @var array
+     */
+
+    protected  $fillable =['key' ,'value'];
+
+    /**
+     * @param array
+     */
+
+    public  static  function  get($key)
+    {
+        $setting = new self();
+        $entry   = $setting->where('key', $key)->first();
+        if(!$entry){
+            return ;
+        }
+        return $entry->value;
+    }
+    /**
+     * @param $key
+     * @param null $value
+     * @return bool
+     */
+    public  static  function  set($key, $value = null)
+    {
+        $setting = new self();
+        $entry   = $setting->where('key', $key)->firstOrFail();
+        $entry->value = $value;
+        $entry->saveOrFail();
+        Config::set('key' , $value);
+
+        if (Config::get($key) == $value){
+
+            return true;
+        }
+
+        return false;
+    }
+}
