fe6d6aeeadf8c451fcf166e93ea53092a55ccc0e
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-15 17:38:24 +0100

    Created a Setting controller and implements to methods on it

---
+++ app/Http/Controllers/AdminSettingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminSettingController extends BaseController
+{
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $this->setPageTitle('Settings', 'Manage Settings');
+        return view('admin.settings.index');
+    }
+}
