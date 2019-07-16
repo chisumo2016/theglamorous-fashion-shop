f9061b49e457d7cae5962310411e3472356eccf1
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-16 13:17:07 +0100

    Added the Categiries Menu to Sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -14,10 +14,14 @@
             </a>
         </li>
         <li>
-{{--            <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-cogs"></i>--}}
-{{--                <span class="app-menu__label">Settings</span>--}}
-{{--            </a>--}}
             <a class="dropdown-item" href="{{ route('admin.settings') }}"><i class="fa fa-cog fa-lg"></i> Settings</a>
+        </li>
+        <li>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.categories.index' ? 'active' : '' }}"
+               href="{{ route('admin.categories.index') }}">
+                <i class="app-menu__icon fa fa-tags"></i>
+                <span class="app-menu__label">Categories</span>
+            </a>
         </li>
     </ul>
 </aside>
