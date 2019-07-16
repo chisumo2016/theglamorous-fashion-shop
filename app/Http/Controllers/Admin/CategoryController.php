9eca9f6a0a7840e0e6192f0a6039dc137e786820
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-16 14:13:36 +0100

    Created Category Contract and Repository

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+
+/**
+ * Class CategoryController
+ * @package App\Http\Controllers\Admin
+ */
+class CategoryController extends BaseController
+{
+    //
+}
