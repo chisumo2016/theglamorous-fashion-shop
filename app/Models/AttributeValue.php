335cf8a811775b0543eb36f71b1e2a93fb52c75c
Bernard Chisumo <bchisumo74@gmail.com>
2019-07-24 13:14:17 +0100

    Added Mass assigement to Attributes Values

--- app/Models/AttributeValue.php
+++ app/Models/AttributeValue.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class AttributeValue extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'attribute_values';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'attribute_id', 'value', 'price'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'attribute_id'  =>  'integer',
+    ];
 }
