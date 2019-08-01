b6c503de7f5735bdc30b8a07c84f1e9b5e56ceef
Bernard Chisumo <bchisumo74@gmail.com>
2019-08-01 14:03:39 +0100

    Created a BrandController and extend to BaseController

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\BaseController;
+use App\Contracts\BrandContract;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BrandController extends BaseController
+{
+    //
+}
