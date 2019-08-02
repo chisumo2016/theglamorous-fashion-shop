f9a23232872306f32667e8869b658b0a6923e46c
Bernard Chisumo <bchisumo74@gmail.com>
2019-08-02 14:14:42 +0100

    Implemented Product Model and Migration successfully.Added Mass Assigement & relationship btn Product and Brand

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'brand_id', 'sku', 'name', 'slug', 'description', 'quantity',
+        'weight', 'price', 'sale_price', 'status', 'featured',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'quantity'  =>  'integer',
+        'brand_id'  =>  'integer',
+        'status'    =>  'boolean',
+        'featured'  =>  'boolean'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
