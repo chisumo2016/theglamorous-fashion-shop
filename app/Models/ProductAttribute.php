39b11e85ce55c55373d7e690c9380fe7e94bb0bf
Bernard Chisumo <bchisumo74@gmail.com>
2019-08-02 15:00:30 +0100

    Product Attributes Model and Migrations implemented successfully

---
+++ app/Models/ProductAttribute.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductAttribute extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'product_attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['product_id', 'quantity', 'price'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function attributes()
+    {
+        return $this->hasMany(ProductAttribute::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function attributesValues()
+    {
+        return $this->belongsToMany(AttributeValue::class);
+    }
+
+}
