492e955417cb531d4435a1313c7c2f63b663e965
Bernard Chisumo <bchisumo74@gmail.com>
2019-08-02 16:35:44 +0100

    Created Product Controller successfully

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Contracts\BrandContract;
+use App\Contracts\CategoryContract;
+use App\Contracts\ProductContract;
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends BaseController
+{
+    protected $brandRepository;
+    protected $categoryRepository;
+    protected $productRepository;
+
+    public function __construct(BrandContract $brandRepository, CategoryContract $categoryRepository, ProductContract $productRepository)
+    {
+        $this->brandRepository = $brandRepository;
+        $this->categoryRepository = $categoryRepository;
+        $this->productRepository = $productRepository;
+    }
+}
