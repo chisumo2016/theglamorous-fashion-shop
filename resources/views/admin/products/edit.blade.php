699d0ed6ff6ea6a3af7b346b2357fd1f19acaa5d
Bernard Chisumo <bchisumo74@gmail.com>
2019-08-08 14:42:53 +0100

    Added Dropzone File Uploader in edit product page

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -252,4 +252,48 @@
             $('#categories').select2();
         });
     </script>
+
+    <script>
+        Dropzone.autoDiscover = false;
+
+        $( document ).ready(function() {
+            $('#categories').select2();
+
+            let myDropzone = new Dropzone("#dropzone", {
+                paramName: "image",
+                addRemoveLinks: false,
+                maxFilesize: 4,
+                parallelUploads: 2,
+                uploadMultiple: false,
+                url: "{{ route('admin.products.images.upload') }}",
+                autoProcessQueue: false,
+            });
+            myDropzone.on("queuecomplete", function (file) {
+                window.location.reload();
+                showNotification('Completed', 'All product images uploaded', 'success', 'fa-check');
+            });
+            $('#uploadButton').click(function(){
+                if (myDropzone.files.length === 0) {
+                    showNotification('Error', 'Please select files to upload.', 'danger', 'fa-close');
+                } else {
+                    myDropzone.processQueue();
+                }
+            });
+            function showNotification(title, message, type, icon)
+            {
+                $.notify({
+                    title: title + ' : ',
+                    message: message,
+                    icon: 'fa ' + icon
+                },{
+                    type: type,
+                    allow_dismiss: true,
+                    placement: {
+                        from: "top",
+                        align: "right"
+                    },
+                });
+            }
+        });
+    </script>
 @endpush
