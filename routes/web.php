2263443587f7dd8b42743f18a158b72e1af01c4e
Bernard Chisumo <bchisumo74@gmail.com>
2019-09-05 12:34:29 +0100

    Implemented the integration of the front end

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::view('/', 'site.pages.homepage');
 
 //Route::view('/admin', 'admin.dashboard.index');
 //Route::view('/admin/login', 'admin.auth.login');
