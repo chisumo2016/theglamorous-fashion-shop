41ff5e56947e1c74afe4aca6e8640d6139766d77
Bernard Chisumo <bchisumo74@gmail.com>
2019-09-23 17:05:11 +0100

    Created Order Contract and Repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -14,6 +14,8 @@ use App\Repositories\BrandRepository;
 
 use App\Contracts\ProductContract;
 use App\Repositories\ProductRepository;
+use App\Contracts\OrderContract;
+use App\Repositories\OrderRepository;
 
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -23,6 +25,7 @@ class RepositoryServiceProvider extends ServiceProvider
         AttributeContract::class        =>     AttributeRepository::class,
         BrandContract::class            =>      BrandRepository::class,
         ProductContract::class          =>      ProductRepository::class,
+        OrderContract::class            =>          OrderRepository::class,
 
     ];
 
