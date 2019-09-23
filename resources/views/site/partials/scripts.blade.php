75923b6d4922c575cf209536682d487961526b74
Bernard Chisumo <bchisumo74@gmail.com>
2019-09-23 13:24:07 +0100

    Installed Shopping Cart Package into Application

--- resources/views/site/partials/scripts.blade.php
+++ resources/views/site/partials/scripts.blade.php
@@ -4,3 +4,5 @@
 <script src="{{ asset('frontend/plugins/fancybox/fancybox.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('frontend/plugins/owlcarousel/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('frontend/js/script.js') }}" type="text/javascript"></script>
+
+@stack('scripts')
